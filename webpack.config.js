4215fa074cc51d4e7877605c4feb79b6a5d00ebb
zhuhao <979263092@qq.com>
2018-04-09 17:12:02 +0800

    zbox-bios-web

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/main.js',
+    mode: 'development',
+    devServer: {
+        port: 8099,
+        historyApiFallback: true,
+        proxy: {
+            // 请求到 '/device' 下 的请求都会被代理到 target： http://debug.xxx.com 中
+            '/zboxService/*': {
+                target: 'http://localhost:8021',
+                secure: false, // 接受 运行在 https 上的服务
+                changeOrigin: true
+            }
+        },
+        inline: true,//注意：不写hot: true，否则浏览器无法自动更新；也不要写colors:true，progress:true等，webpack2.x已不支持这些
+    },
+    output: {
+        path: path.join(__dirname, 'dist'), //输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它
+        filename: "bundle.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        rules: []
+    }
+};
