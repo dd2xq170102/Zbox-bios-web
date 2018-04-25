16562c14bdb3bb2922cc04e4cec2c975a72ded4f
zhuhao <979263092@qq.com>
2018-04-25 09:16:55 +0800

    VUE

---
+++ src/modules/util/util.vue
@@ -0,0 +1,23 @@
+/**
+* Created by zhuhao on 2018/4/24.
+*/
+<template>
+    <section>
+
+    </section>
+</template>
+<script>
+    import $ from 'jquery'
+
+    export default {
+        data(){
+            return {}
+        },
+        created: function () {
+
+        },
+        methods: function () {
+
+        }
+    }
+</script>
